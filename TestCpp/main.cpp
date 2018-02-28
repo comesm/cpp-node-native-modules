7e301500f856547f19719347338127041d373789
Michael <comesm@gmail.com>
2018-02-27 20:39:59 -0500

    Initial Commit

---
+++ TestCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestCpp
+//
+//  Created by Michael Comes on 2/27/18.
+//  Copyright © 2018 Michael Comes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
